d6032811dcbae5b38f552ca5a0883900cd91bb0c
const-takeo <const.takeo@gmail.com>
2021-06-16 00:47:51 +0900

    Project Setup

---
+++ src/main/java/takeo/spring_basic/controller/HelloController.java
@@ -0,0 +1,16 @@
+package takeo.spring_basic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello");
+        return "hello";
+    }
+}
