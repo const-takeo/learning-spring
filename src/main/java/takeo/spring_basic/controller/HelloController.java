643a5975de7c470a264bab0d3425b4bea4b3baef
const-takeo <const.takeo@gmail.com>
2021-06-16 10:03:43 +0900

    How to work @ResponseBody

--- src/main/java/takeo/spring_basic/controller/HelloController.java
+++ src/main/java/takeo/spring_basic/controller/HelloController.java
@@ -4,19 +4,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello");
         return "hello";
     }
-    //mvc pattern
-    @GetMapping("/hello-mvc")
-    public String helloString(@RequestParam("name") String name, Model model) {
+
+    // mvc pattern
+    @GetMapping("hello-mvc")
+    public String helloMVC(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
     }
+
+    // string
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+
 }
